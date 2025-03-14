56589f78068b44e557f320211afcd6ca4a0eb7ca
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-03-14 22:05:28 +0000

    Updated Header

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container text-center">
-      <h1>Welcome to Omkar Portfolio!</h1>
+      <h1>Hello to Omkar Portfolio!</h1>
       <p>Hi, I'm [Your Name]. I’m a Full-Stack Developer.</p>
     </div>
   );

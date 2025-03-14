3aa5a3b4b324fe3731fb78087a3cf17c8531a7a9
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-03-14 23:39:01 +0000

    Updated Contact Info

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
             <Link className="nav-link" to="/projects">Projects</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
+            <Link className="nav-link" to="/contact">Contact Info</Link>
           </li>
         </ul>
       </div>

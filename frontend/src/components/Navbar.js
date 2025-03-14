2536b71328897dc26d8d5062a8d4a2e8e16d719b
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-03-14 21:26:55 +0000

    Updated App.js and Header.js components

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
             <Link className="nav-link" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/projects">Projects</Link>
+            <Link className="nav-link" to="/projects">Project</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/contact">Contact</Link>

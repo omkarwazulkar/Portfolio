3bbcd379c9d3d1c513b5d79329a24514620f03a5
Omkar Wazulkar <omkarwazulkar@Omkars-MacBook-Air.local>
2025-03-15 00:20:35 +0000

    Update B/F

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // MongoDB Connection (We will set this in the next step)
 mongoose.connect(process.env.MONGO_URI, {
